331fec41224dde5a5758083fdfc818893b51e5e0
Viktor Stepanov <vitya.zap@gmail.com>
2022-10-01 15:01:30 +0300

    Refactor fixGibberish()

--- index.js
+++ index.js
@@ -1,287 +1,30 @@
-const ukrainian = `ЙйЦцУуКкЕеНнГгШшЩщЗзХхЇїФфІіВвАаПпРрОоЛлДдЖжЄєЯяЧчСсМмИиТтЬьБбЮю,.`
-const russian   = `ЙйЦцУуКкЕеНнГгШшЩщЗзХхЪъФфЫыВвАаПпРрОоЛлДдЖжЄєЯяЧчСсМмИиТтЬьБбЮю,.`
-const english   = `QqWwEeRrTtYyUuIiOoPp{[}]AaSsDdFfGgHhJjKkLl:;"'ZzXxCcVvBbNnMm<,>.?/`
+const ukrainian = `ЙйЦцУуКкЕеНнГгШшЩщЗзХхЇїФфІіВвАаПпРрОоЛлДдЖжЄєЯяЧчСсМмИиТтЬьБбЮю,.`;
+const russian   = `ЙйЦцУуКкЕеНнГгШшЩщЗзХхЪъФфЫыВвАаПпРрОоЛлДдЖжЄєЯяЧчСсМмИиТтЬьБбЮю,.`;
+const english   = `QqWwEeRrTtYyUuIiOoPp{[}]AaSsDdFfGgHhJjKkLl:;"'ZzXxCcVvBbNnMm<,>.?/`;
 
 function fixGibberish(string, from = english, to = ukrainian) {
   let result = [];
+  let searchValue;
   for (let i = 0; i < string.trim().split('').length; i++) {
-    switch (string.trim().split('')[i]) {
-      case from[0]: {
-        result[i] = to[0];
+    switch (string[i]) { // conflicting chars with RegEx in search(): [ ? .
+      case '[': {
+        searchValue = from[21];
         break;
       }
-      case from[1]: {
-        result[i] = to[1];
+      case '.': {
+        searchValue = from[63];
         break;
       }
-      case from[2]: {
-        result[i] = to[2];
+      case '?': {
+        searchValue = from[64];
         break;
       }
-      case from[3]: {
-        result[i] = to[3];
+      default: {
+        searchValue = from.search(string[i]); // use RegEx here..?
         break;
       }
-      case from[4]: {
-        result[i] = to[4];
-        break;
-      }
-      case from[5]: {
-        result[i] = to[5];
-        break;
-      }
-      case from[6]: {
-        result[i] = to[6];
-        break;
-      }
-      case from[7]: {
-        result[i] = to[7];
-        break;
-      }
-      case from[8]: {
-        result[i] = to[8];
-        break;
-      }
-      case from[9]: {
-        result[i] = to[9];
-        break;
-      }
-      case from[10]: {
-        result[i] = to[10];
-        break;
-      }
-      case from[11]: {
-        result[i] = to[11];
-        break;
-      }
-      case from[12]: {
-        result[i] = to[12];
-        break;
-      }
-      case from[13]: {
-        result[i] = to[13];
-        break;
-      }
-      case from[14]: {
-        result[i] = to[14];
-        break;
-      }
-      case from[15]: {
-        result[i] = to[15];
-        break;
-      }
-      case from[16]: {
-        result[i] = to[16];
-        break;
-      }
-      case from[17]: {
-        result[i] = to[17];
-        break;
-      }
-      case from[18]: {
-        result[i] = to[18];
-        break;
-      }
-      case from[19]: {
-        result[i] = to[19];
-        break;
-      }
-      case from[20]: {
-        result[i] = to[20];
-        break;
-      }
-      case from[21]: {
-        result[i] = to[21];
-        break;
-      }
-      case from[22]: {
-        result[i] = to[22];
-        break;
-      }
-      case from[23]: {
-        result[i] = to[23];
-        break;
-      }
-      case from[24]: {
-        result[i] = to[24];
-        break;
-      }
-      case from[25]: {
-        result[i] = to[25];
-        break;
-      }
-      case from[26]: {
-        result[i] = to[26];
-        break;
-      }
-      case from[27]: {
-        result[i] = to[27];
-        break;
-      }
-      case from[28]: {
-        result[i] = to[28];
-        break;
-      }
-      case from[29]: {
-        result[i] = to[29];
-        break;
-      }
-      case from[30]: {
-        result[i] = to[30];
-        break;
-      }
-      case from[31]: {
-        result[i] = to[31];
-        break;
-      }
-      case from[32]: {
-        result[i] = to[32];
-        break;
-      }
-      case from[33]: {
-        result[i] = to[33];
-        break;
-      }
-      case from[34]: {
-        result[i] = to[34];
-        break;
-      }
-      case from[35]: {
-        result[i] = to[35];
-        break;
-      }
-      case from[36]: {
-        result[i] = to[36];
-        break;
-      }
-      case from[37]: {
-        result[i] = to[37];
-        break;
-      }
-      case from[38]: {
-        result[i] = to[38];
-        break;
-      }
-      case from[39]: {
-        result[i] = to[39];
-        break;
-      }
-      case from[40]: {
-        result[i] = to[40];
-        break;
-      }
-      case from[41]: {
-        result[i] = to[41];
-        break;
-      }
-      case from[42]: {
-        result[i] = to[42];
-        break;
-      }
-      case from[43]: {
-        result[i] = to[43];
-        break;
-      }
-      case from[44]: {
-        result[i] = to[44];
-        break;
-      }
-      case from[45]: {
-        result[i] = to[45];
-        break;
-      }
-      case from[46]: {
-        result[i] = to[46];
-        break;
-      }
-      case from[47]: {
-        result[i] = to[47];
-        break;
-      }
-      case from[48]: {
-        result[i] = to[48];
-        break;
-      }
-      case from[49]: {
-        result[i] = to[49];
-        break;
-      }
-      case from[50]: {
-        result[i] = to[50];
-        break;
-      }
-      case from[51]: {
-        result[i] = to[51];
-        break;
-      }
-      case from[52]: {
-        result[i] = to[52];
-        break;
-      }
-      case from[53]: {
-        result[i] = to[53];
-        break;
-      }
-      case from[54]: {
-        result[i] = to[54];
-        break;
-      }
-      case from[55]: {
-        result[i] = to[55];
-        break;
-      }
-      case from[56]: {
-        result[i] = to[56];
-        break;
-      }
-      case from[57]: {
-        result[i] = to[57];
-        break;
-      }
-      case from[58]: {
-        result[i] = to[58];
-        break;
-      }
-      case from[59]: {
-        result[i] = to[59];
-        break;
-      }
-      case from[60]: {
-        result[i] = to[60];
-        break;
-      }
-      case from[61]: {
-        result[i] = to[61];
-        break;
-      }
-      case from[62]: {
-        result[i] = to[62];
-        break;
-      }
-      case from[63]: {
-        result[i] = to[63];
-        break;
-      }
-      case from[64]: {
-        result[i] = to[64];
-        break;
-      }
-      case from[65]: {
-        result[i] = to[65];
-        break;
-      }
-      case from[66]: {
-        result[i] = to[66];
-        break;
-      }
-      // case from[]: {
-      //   result[i] = to[];
-      //   break;
-      // }
-      default:
-        result[i] = string.trim().split('')[i];
-        break;
     }
+    result[i] = searchValue === -1 ? string.trim().split('')[i] : to[searchValue];
   }
   return result.join('');
 }
